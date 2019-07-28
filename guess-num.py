2d70d0c08c09310125af4d5aa14347bef4b7ab94
sophie chen <pandoaki@gmial.com>
2019-07-28 16:24:18 +0800

    add count

--- guess-num.py
+++ guess-num.py
@@ -1,8 +1,11 @@
 import random #載入模組
 r = random.randint(1, 100)
-while True:
+count = 0
+while True:	
 	num = input('請猜數字：')
 	num = int(num) #型別轉換（casting)
+	count += 1 #count =count + 1
+	print('目前猜第', count,'次')
 	if num == r:
 		print('終於猜對了！！！')
 		break
@@ -10,3 +13,5 @@ while True:
 		print('比答案大喲')
 	else:
 		print('比答案小喲')
+	
+	
