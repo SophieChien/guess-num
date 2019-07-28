22d02ba7b53aa7cc38c8b97039ba44ff15ede2fa
sophie chen <pandoaki@gmial.com>
2019-07-28 16:00:49 +0800

    update file

--- guess-num.py
+++ guess-num.py
@@ -1,12 +1,12 @@
+import random #載入模組
+r = random.randint(1, 100)
 while True:
-	import random
-	r = random.randint(1, 5)
 	num = input('請猜數字：')
-	num = int(num). #型別轉換（casting)
+	num = int(num) #型別轉換（casting)
 	if num == r:
 		print('終於猜對了！！！')
 		break
 	elif num > r:
-		print('比答案大喲,答案是', r)
+		print('比答案大喲')
 	else:
-		print('比答案小喲,答案是', r)
+		print('比答案小喲')
