65bbcacce745ad8f5985bee50ce8278201858cf2
sophie chen <pandoaki@gmial.com>
2019-07-28 16:36:25 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -1,17 +1,21 @@
 import random #載入模組
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值：')
+end = input('請決定隨機數字範圍結束值：')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:	
 	num = input('請猜數字：')
 	num = int(num) #型別轉換（casting)
 	count += 1 #count =count + 1
-	print('目前猜第', count,'次')
+	print('目前猜第', count,'次,')
 	if num == r:
 		print('終於猜對了！！！')
 		break
 	elif num > r:
-		print('比答案大喲')
+		print('比答案大喲.')
 	else:
-		print('比答案小喲')
+		print('比答案小喲.')
 	
 	
